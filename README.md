f7ffcfb31f8bcd2df89322cbed001fc33e147dcb
kartik-sajwan <kartik.sajwan@gmail.com>
2021-06-22 09:46:07 +0530

    final commit

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # human-pose-estimation-opencv
-Perform Human Pose Estimation in OpenCV Using OpenPose MobileNet
+Perform Human Pose Estimation in OpenCV
 
 ![OpenCV Using OpenPose MobileNet](output.png)
 ![OpenCV Using OpenPose MobileNet](output.gif)
@@ -17,7 +17,7 @@ pyhton -m venv env
 ```
 source env/bin/activate 
 ``` 
-- Install requirements with pi;
+- Install requirements with pip
 
 ```
 pip install -r requirement.txt
@@ -25,24 +25,8 @@ pip install -r requirement.txt
 
 # Test with following commands
 
-- Test with webcam
+- Test with video
 
 ```
-python openpose.py
+python ai_trainer.py
 ```
-
-- Test with image
-
-```
-python openpose.py --input image.jpg
-```
-
-- Use `--thr` to increase confidence threshold
-
-```
-python openpose.py --input image.jpg --thr 0.5
-```
-
-# Notes:
-- This project makes use of [OpenCV DNN Example](https://github.com/opencv/opencv/blob/master/samples/dnn/openpose.py) to use the `Tensorflow MobileNet Model`, which is provided by [ildoonet/tf-pose-estimation](https://github.com/ildoonet/tf-pose-estimation/tree/master/models/graph/mobilenet_thin), instead of `Caffe Model` from CMU OpenPose. The original `openpose.py` from `OpenCV example` only uses `Caffe Model` which is more than 200MB while the `Mobilenet` is only 7MB.
-- Basically, we need to change the `cv.dnn.blobFromImage` and use `out = out[:, :19, :, :]` to get only the first 19 rows in the `out` variable.
--- ai_trainer.py
+++ ai_trainer.py
@@ -4,7 +4,7 @@ import time
 import PoseModule as pm
 
 # cap = cv2.VideoCapture()
-cap = cv2.VideoCapture('1.mp4')
+cap = cv2.VideoCapture("curls.mp4")
 detector = pm.poseDetector()
 count = 0
 dir = 0
@@ -20,7 +20,7 @@ while True:
         # Right Arm
         angle = detector.findAngle(img, 12, 14, 16)
         # # Left Arm
-        angle = detector.findAngle(img, 11, 13, 15,False)
+        angle = detector.findAngle(img, 11, 13, 15, False)
         per = np.interp(angle, (210, 310), (0, 100))
         bar = np.interp(angle, (220, 310), (650, 100))
         print(angle, per)
@@ -42,19 +42,22 @@ while True:
         # Draw Bar
         cv2.rectangle(img, (1100, 100), (1175, 650), color, 3)
         cv2.rectangle(img, (1100, int(bar)), (1175, 650), color, cv2.FILLED)
-        cv2.putText(img, f'{int(per)} %', (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4,
-                    color, 4)
+        cv2.putText(
+            img, f"{int(per)} %", (1100, 75), cv2.FONT_HERSHEY_PLAIN, 4, color, 4
+        )
 
         # Draw Curl Count
         cv2.rectangle(img, (0, 450), (250, 720), (0, 255, 0), cv2.FILLED)
-        cv2.putText(img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15,
-                    (255, 0, 0), 25)
+        cv2.putText(
+            img, str(int(count)), (45, 670), cv2.FONT_HERSHEY_PLAIN, 15, (255, 0, 0), 25
+        )
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
-    cv2.putText(img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5,
-                (255, 0, 0), 5)
+    cv2.putText(
+        img, str(int(fps)), (50, 100), cv2.FONT_HERSHEY_PLAIN, 5, (255, 0, 0), 5
+    )
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
